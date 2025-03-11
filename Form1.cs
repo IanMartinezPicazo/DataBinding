b495451c1fc61e2bff21c4601685eaa2200d6393
IanMartinezPicazo <iama249@vidalibarraquer.net>
2025-03-11 09:01:37 +0100

    Inicialització de control de versió

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Conversor_de_divises___Ian_Martínez_Picazo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
